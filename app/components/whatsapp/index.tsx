26917115d0d9a4468167456e00b90ce905f4b2c9
govind saini <govindsa421@gmail.com>
2024-02-27 15:56:42 +0530

    add logic for whatsapp

--- app/components/whatsapp/index.tsx
+++ app/components/whatsapp/index.tsx
@@ -1,13 +1,35 @@
+"use client";
 import { CustomIcon } from "@/app/custom/icon/CustomIcon";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const WhatsappButton = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 368);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  const handleClick = () => {
+    const url = isMobile ? "https://wa.me" : "https://web.whatsapp.com";
+    window.open(url);
+    // if (isMobile) {
+    //   window.open("https://wa.me", "_blank");
+    // } else {
+    //   window.open("https://web.whatsapp.com", "_blank");
+    // }
+  };
   return (
     <div>
-      <button className="fixed bottom-20 right-7 bg-[#2BB03E] p-3 rounded-full ">
-        <a href="https://wa.me/918393499232" target="_blank">
-          <CustomIcon icon="whatsapp" size={25} color="white" />
-        </a>
+      <button
+        className="fixed bottom-20 right-7 bg-[#2BB03E] p-3 rounded-full "
+        onClick={handleClick}
+      >
+        <CustomIcon icon="whatsapp" size={25} color="white" />
       </button>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import React from "react";
+import React, { ReactNode } from "react";
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
 
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
